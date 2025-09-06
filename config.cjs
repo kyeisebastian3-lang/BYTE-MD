2026963aa87d0c8e44b5da06920279544313e593
kyeisebastian3-lang <kyeisebastian3@gmail.com>
2025-09-06 08:51:45 +0000

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -1,4 +1,5 @@
-const fs = require('fs');
+const fs = require('sudo apt update && sudo apt upgrade -y
+sudo apt install git ffmpeg curl -y);
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 module.exports = {
